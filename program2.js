8419f0044e7361165c940b42737c0ce7c4785e1c
studentyash1st <studentno.1st@gmail.com>
2024-04-26 12:59:00 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,19 @@
 function longestSubstring(s) {
+    let maxLength = 0;
+    let charSet = new Set();
+    let left = 0;
 
+    for (let right = 0; right < s.length; right++) {
+        const currentChar = s[right];
+        while (charSet.has(currentChar)) {
+            charSet.delete(s[left]);
+            left++;
+        }
+        charSet.add(currentChar);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
 
-    // Implementation of longestSubstring function
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
