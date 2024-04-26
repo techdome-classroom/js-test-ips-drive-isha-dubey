1d9d5e933e0479638efe72c117c44365da40f4f6
studentyash1st <studentno.1st@gmail.com>
2024-04-26 12:26:02 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -11,7 +11,7 @@ function smallestMissingPositiveInteger(nums) {
     if(num === smallestNum){
       smallestNum++
     }
-    else if (num > smallest) {
+    else if (num > smallestNum) {
       return smallestNum
     }
    }
