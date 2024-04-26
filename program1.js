41eb8797612af4a9d069465ae7963ab07122bc1d
studentyash1st <studentno.1st@gmail.com>
2024-04-26 12:39:35 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -41,9 +41,10 @@ function smallestMissingPositiveInteger(nums) {
     // Check if all tests passed
     const allPassed = results.every(result => result);
     console.log("All tests passed:", allPassed ? "Yes" : "No")
-    
+
     window.onload = function() {
       runTestCases();
+      
   module.exports = smallestMissingPositiveInteger;
 
   }
